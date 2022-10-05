512e394fb514da86859e81399c5456f6c25c8bd0
Lucien Rondier <103604432+lucien92@users.noreply.github.com>
2022-10-05 16:38:28 +0200

    update LSTM

---
+++ evaluate.py
@@ -0,0 +1,3 @@
+#Pour évaluer un auto-encodeur il faut enlever la partie espace latent/décodeur, poser un classifieur (couche dense ou autre) et vérifier sur un
+#ensemble de données annotées si le classifieur est capable de prédire la classe de l'image.
+
---
+++ kmeans.py
@@ -0,0 +1,2 @@
+#On cherche à appliquer un algorithme de clustering sur les différentes anomalies détectées par l'auto-encodeur.
+
